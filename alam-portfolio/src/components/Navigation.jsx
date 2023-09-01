9cae72fce572fc4fc81e8e0ed49eedf886f6730f
alam2tg <aaronlamfinance@gmail.com>
2023-08-31 23:13:01 -0700

    Add pseudocode in Navigation.jsx

--- alam-portfolio/src/components/Navigation.jsx
+++ alam-portfolio/src/components/Navigation.jsx
@@ -1,1 +1,10 @@
 // Navigation component within the header that will be used to render the different sections of your portfolio
+// Add titles corresponding to the sections About Me, Portfolio, Contact, and Resume. Title for corresponding selection is highlighted in the navigation bar
+	// About Me Section: Include recent photo of self
+	// Portfolio Section: Include list of 6 projects with link to corresponding repo
+	// Contact Section: Contact form with fields for name, email, and message.
+		// When curse moved out of form fields without entering text, alert required field
+
+	// Resume Section: Downloadable link to resume
+
+// Footer include linkedin, github, 3rd platform
