d946af4c006d8d86109945d4e00ac676fd0589a9
alam2tg <aaronlamfinance@gmail.com>
2023-09-01 00:22:12 -0700

    Add header.jsx + style

--- alam-portfolio/src/components/Header.jsx
+++ alam-portfolio/src/components/Header.jsx
@@ -1,1 +1,22 @@
 // Header component used on multiple pages
+
+import '../styles/Header.css'
+
+const styles = {
+	headerStyle: {
+		background: '#5d72e9',
+	},
+	headingStyle: {
+		fontSize: '64px',
+	}
+};
+
+function Header() {
+	return(
+		<header style={styles.headerStyle} className='header'>
+			<h1 style={styles.headingStyle}>Aaron's Portfolio</h1>
+		</header>
+	);
+}
+
+export default Header;
