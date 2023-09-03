da2ce0f66601db8c2cc2d43584098f001ce89266
alam2tg <aaronlamfinance@gmail.com>
2023-09-03 10:31:08 -0700

    Add styling + hero img to about

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -14,11 +14,11 @@ const styles = {
 
 export default function Portfolio() {
 	return (
-		<div className="Card Projects-Container"  style={styles.content}>
+		<div style={styles.content}>
 			<h1 >
 				My Projects
 			</h1>
-			<p style='styles.card'>
+			<p style={styles.card}>
 				Here is a collection of some of the projects I have worked on in my time as a developer. The goal of these projects was to apply the new skills I have learned while coding. Please feel free to reach out in the Contact section if you have any questions!
 			</p>
 		</div>
