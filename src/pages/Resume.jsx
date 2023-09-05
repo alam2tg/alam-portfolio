57366608b0c1d2e498e9286af028c6e4d7974241
alam2tg <aaronlamfinance@gmail.com>
2023-09-04 23:56:54 -0700

    Bug fixes

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -6,8 +6,22 @@ export default function Resume() {
 	return (
 		<div>
 			<h1>Resume</h1>
-
-			<a href="https://docs.google.com/document/d/1sxQncK5YudbjqCxupKw_lHZIc43ngg41wUeUePgm4fg/edit?usp=sharing">Download</a>
+			<p>Please click the link below to view a downloadable copy of my resume!</p>
+			<h3>AARON LAM </h3>
+			<h3>Web Developer</h3>
+			<strong></strong>
+			<p></p>
+			<strong></strong>
+			<p></p>
+			<strong></strong>
+			<p></p>
+			<strong></strong>
+			<p></p>
+			<strong></strong>
+			<p></p>
+			<strong></strong>
+			<p></p>
+			<a href="https://docs.google.com/document/d/1sxQncK5YudbjqCxupKw_lHZIc43ngg41wUeUePgm4fg/edit?usp=sharing"><strong>Download Here!</strong></a>
 		</div>
 	)
 }
