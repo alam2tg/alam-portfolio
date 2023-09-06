48e1c3d1afb68ff1c52e1beed9677a22cb8aecb0
alam2tg <aaronlamfinance@gmail.com>
2023-09-05 21:29:50 -0700

    Add downloadable resume + images to portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,12 +1,17 @@
 // add 6 active repos to portfolio
 // export default function
 import Projects from "../components/Projects";
+import dddLogo from "../images/dddLogo.jpg"
+import enDEVors from "../images/enDEVors.png"
+import placeholder from "../images/placeholder.png"
+
 
 const project = [
 	{
 		id: 1,
 		title: "EnDEVors",
 		description: "",
+		image: enDEVors,
 		website: "https://endevor-3d604fa0490c.herokuapp.com/",
 		github: "https://github.com/ssherp/EnDEVor",
 	},
@@ -14,6 +19,7 @@ const project = [
 		id: 2,
 		title: "Specialized Fuel Locator",
 		description: "This application searches for nearby gas stations by Zip Code and fuel type to return a list of nearby gas stations with their address",
+		image: dddLogo,
 		website: "https://iquiroz95.github.io/diesel-din-dance/",
 		github: "https://github.com/iquiroz95/diesel-din-dance",
 	},
@@ -21,6 +27,7 @@ const project = [
 		id: 3,
 		title: "Project 3 Calendar",
 		description: "This application allows a team to create a shared calendar for a group/team to create events, view schedules, and schedule meetings more effectively by avoiding scheduling conflicts",
+		image: placeholder,
 		website: "",
 		github: "https://github.com/alam2tg/Calendar-Schedule-Sync",
 	},
@@ -28,6 +35,7 @@ const project = [
 		id: 4,
 		title: "Social Network API",
 		description: "There is no deployed site for this project. This project was intended to test relational database concepts with MongoDB.",
+		image: placeholder,
 		website: "https://github.com/alam2tg/social-network-api",
 		github: "https://github.com/alam2tg/social-network-api",
 	},
@@ -35,6 +43,7 @@ const project = [
 		id: 5,
 		title: "Reverse Skull Key",
 		description: "This application is a simple password generator that uses window prompts to accept",
+		image: placeholder,
 		website: "https://alam2tg.github.io/reverse-skull-key/",
 		github: "https://github.com/alam2tg/reverse-skull-key",
 	},
@@ -42,6 +51,7 @@ const project = [
 		id: 6,
 		title: "Aaron's Time Capsule",
 		description: "My first personal website using HTML, CSS, and Flexbox",
+		image: placeholder,
 		website: "https://alam2tg.github.io/alamtimecapsule/",
 		github: "https://github.com/alam2tg/alamtimecapsule",
 	},
@@ -69,9 +79,13 @@ export default function Project() {
 					Here is a collection of some of the projects I have worked on in my time as a developer. The goal of these projects was to apply the new skills I have learned while coding. Please feel free to reach out in the Contact section if you have any questions!
 				</p>
 			</div>
-			<div>
+			<div className="ProjectsCard">
+				<div className='img-container'>
+                    <div className="project-image">
+                        <img src={currentProject.image} alt={currentProject.title} />
+                    </div>
 				<ul className="project-list">
-					return< Projects project={project}/>
+					< Projects project={project}/>
 				</ul>
 			</div>
 		</div>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,6 @@
 // create downloadable resume link
 // export default function
+import AaronWebDevResume from '../styles/files/AaronWebDevResume.pdf'
 
 
 export default function Resume() {
@@ -9,19 +10,9 @@ export default function Resume() {
 			<p>Please click the link below to view a downloadable copy of my resume!</p>
 			<h3>AARON LAM </h3>
 			<h3>Web Developer</h3>
-			<strong></strong>
-			<p></p>
-			<strong></strong>
-			<p></p>
-			<strong></strong>
-			<p></p>
-			<strong></strong>
-			<p></p>
-			<strong></strong>
-			<p></p>
-			<strong></strong>
-			<p></p>
-			<a href="https://docs.google.com/document/d/1sxQncK5YudbjqCxupKw_lHZIc43ngg41wUeUePgm4fg/edit?usp=sharing"><strong>Download Here!</strong></a>
+
+			<a href={AaronWebDevResume} download='AaronWebDevResume.pdf'>Download Here!</a>
+
 		</div>
 	)
 }
